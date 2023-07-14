30e1c8375fba031048a981801519ce845db35d92
nguyenluc97 <louiskingvn@gmail.comlouiskingvn@gmail.comlouiskingvn@gmail.com>
2023-07-14 17:42:24 +0700

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        String[] arr = {"aw", "aa", "dd", "ee", "oo", "ow", "w"};
-        int count = 0;
-        boolean checkEmpty = false;
-
-        Scanner sc = new Scanner(System.in);
         System.out.println("Nhap vao chuoi ki tu can kiem tra:");
         StringBuilder str = new StringBuilder(sc.nextLine());
-
-        StringBuilder rs = new StringBuilder("(");
         //check empty
-        if (str.isEmpty()) {
-            checkEmpty = true;
-        }
-        while (checkEmpty) {
-            System.out.println("Nhap lai chuoi ki tu can kiem tra:");
-            str = new StringBuilder(sc.nextLine());
-            if (!str.isEmpty()) {
-                checkEmpty = false;
-            }
-        }
+        str = checkEmpty(str);
+        getResult(str);
+    }
 
-        //so sanh chuoi da nhap vao voi mang
+    private static StringBuilder checkEmpty(StringBuilder str) {
+        do {
+            if (str.isEmpty()) {
+                System.out.println("Nhap lai chuoi ki tu can kiem tra:");
+                str = new StringBuilder(sc.nextLine());
+            }
+        } while (str.isEmpty());
+        return str;
+    }
+
+    private static void getResult(StringBuilder str) {
+        String[] arr = {"aw", "aa", "dd", "ee", "oo", "ow", "w"};
+        int count = 0;
+        StringBuilder rs = new StringBuilder("(");
         for (String s : arr) {
             if (str.toString().contains(s)) {
-                str.delete(str.indexOf(s), s.length());
                 count++;
                 rs.append(s);
                 rs.append(",");
+                str.delete(str.indexOf(s), str.indexOf(s) + s.length());
             }
         }
         rs.replace(rs.length() - 1, rs.length(), ")");
-        System.out.println(count + " " + rs);
+        System.out.println( count + " " + rs);
     }
+
 }
